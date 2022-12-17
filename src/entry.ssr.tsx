3c1ffac53cc57dc0d3c7b711c4819aaf6a95e4aa
MadeByZeAman28 <MadeByZeAmanuel28@gmail.com>
2022-12-17 10:31:06 +0000

    Fix Dev Mode

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -1,35 +1,14 @@
-import { renderToStream } from "@builder.io/qwik/server";
+import { renderToStream, RenderToStreamOptions } from "@builder.io/qwik/server";
 import { manifest } from "@qwik-client-manifest";
 import Root from "./root";
 
-export default {
-  async fetch(): Promise<Response> {
-    const { writable, readable } = new TransformStream();
-    const writer = writable.getWriter();
-
-    const stream = {
-      write: (chunk: any) => {
-        if (typeof chunk === "string") {
-          const encoder = new TextEncoder();
-          writer.write(encoder.encode(chunk));
-        } else {
-          writer.write(chunk);
-        }
-      },
-    };
-
-    renderToStream(<Root />, {
-      manifest,
-      containerAttributes: {
-        lang: "en-us",
-      },
-      stream,
-    }).finally(() => writer.close());
-
-    return new Response(readable, {
-      headers: {
-        "Content-Type": "text/html; charset=utf-8",
-      },
-    });
-  },
-};
+export default function (opts: RenderToStreamOptions) {
+  return renderToStream(<Root />, {
+    manifest,
+    ...opts,
+    containerAttributes: {
+      lang: "en-us",
+      ...opts.containerAttributes,
+    },
+  });
+}
---
+++ src/entry.worker.tsx
@@ -0,0 +1,35 @@
+import renderToStream from "./entry.ssr";
+
+export default {
+  async fetch(): Promise<Response> {
+    const { writable, readable } = new TransformStream();
+    const writer = writable.getWriter();
+
+    const stream = {
+      write: (chunk: any) => {
+        if (typeof chunk === "string") {
+          const encoder = new TextEncoder();
+          writer.write(encoder.encode(chunk));
+        } else {
+          writer.write(chunk);
+        }
+      },
+    };
+
+    renderToStream({ stream }).finally(() => writer.close());
+
+    /* renderToStream(<Root />, {
+      manifest,
+      containerAttributes: {
+        lang: "en-us",
+      },
+      stream,
+    }).finally(() => writer.close()); */
+
+    return new Response(readable, {
+      headers: {
+        "Content-Type": "text/html; charset=utf-8",
+      },
+    });
+  },
+};
